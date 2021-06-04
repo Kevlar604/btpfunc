989920b43e17fdd4c6881789622fe34b2933dee5
Kevlar604 <just4torrents@protonmail.com>
2021-06-04 05:41:50 +0530

    Update btpfunc.py

--- btpfunc.py
+++ btpfunc.py
@@ -1,10 +1,5 @@
 import vapoursynth as vs
 core = vs.get_core()
-
-path=r'D:\Sync\Walking the Edge 1985 1080p Blu-ray AVC DTS-HD MA 2.0-MM\BDMV\STREAM\00000.m2ts'
-src=core.lsmas.LWLibavSource(path)
-src=core.std.CropRel(src,0,0,20,20)
-src=core.fb.FillBorders(src,0,1,0,1)
 import functools
 def FrameTypeDeband(n,clip,y1,y2,y3,cr1,cr2,cr3,cb1,cb2,cb3):
 	if clip.get_frame(n).props._PictType.decode() == "B":
