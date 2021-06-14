648fc1f62e9031f505f78b8d56036f4c51541362
Kevlar604 <just4torrents@protonmail.com>
2021-06-14 08:38:51 +0530

    Update btpfunc.py

--- btpfunc.py
+++ btpfunc.py
@@ -10,4 +10,9 @@ def FrameTypeDeband(n,clip,y1=0,y2=0,y3=0,cr1=0,cr2=0,cr3=0,cb1=0,cb2=0,cb3=0,gy
 		return core.neo_f3kdb(clip, y=y3, cr=cr3, cb=cb3, grainy=gy3, grainc=gc3,keep_tv_range=True, dynamic_grain=dynamic_grain)
 		
 def ConditionalDeband(clip,y1=0,y2=0,y3=0,cr1=0,cr2=0,cr3=0,cb1=0,cb2=0,cb3=0,gy1=0,gy2=0,gy3=0,gc1=0,gc2=0,gc3=0,dynamic_grain=False):
-		return core.std.FrameEval(clip, functools.partial(FrameTypeDeband,clip=clip,y1=y1,y2=y2,y3=y3,cr1=cr1,cr2=cr2,cr3=cr3,cb1=cb1,cb2=cb2,cb3=cb3,gy1=gy1,gy2=gy2,gy3=gy3,gc1=gc1,gc2=gc2,gc3=gc3,dynamic_grain=dynamic_grain))
+	try:
+		clip=core.std.FrameEval(clip, functools.partial(FrameTypeDeband,clip,y1,y2,y3,cr1,cr2,cr3,cb1,cb2,cb3,gy1,gy2,gy3,gc1,gc2,gc3,dynamic_grain)
+	except Exception:
+		pass
+	return clip						
+					
